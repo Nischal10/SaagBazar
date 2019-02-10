258f1f6f07cd2043a2d1249a62eca6636d93f2e6
Poojan-98 <chapagain.pujan@gmail.com>
2019-02-10 17:57:53 +0545

    pujan's commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-<img src="{{asset('images/SaagBazar.png')}}">
+
+<div class=section-0>
+	<div class="description">
+		<img src="{{asset('images/SaagBazar.png')}}" style="padding:25px;">
+		<div class="first-description define"><p>With over 70% of the Urban Population having access to the internet.<br>It was no brainstormer that Saag Bazer was the direction to go</p></div>
+		<div class="second-description define">
+			<p> We provide a  digital platform for the customers to buy fresh grocery at market price.<br>Along with the ability to have Home Delivery</p>
+		</div>
+		<div class="third-description define">
+			<p> Our current Services Include within Kathmandu Valley Which will soon be Expanded.</p>
+		</div>
+		<div class="forth-description define">
+			<p> Have Fun Shopping</p>
+		</div>
+	</div>
+
+</div>
+
 
 @endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,5 +2,45 @@
 
 @section('content')
 
-
+<section>
+           
+            <div id="team">
+      
+                <div id="team-center">
+                    
+                    <article class="team-member">
+                        <img src="{{asset('images/kishor.png')}}" alt="team-member">
+                        <div class="img-footer">
+                            <div class="footer-info">
+                                <h3>Kishor Kumar Khadka</h3>
+                                <h5>Designer</h5>
+                                
+                            </div>
+                        </div>
+                        
+                    </article>
+                        
+                    <article class="team-member">
+                        <img src="{{asset('images/Pujan1.jpg')}}"  alt="team-member">
+                        <div class="img-footer">
+                            <div class="footer-info">
+                                <h3>Pujan Chapagain</h3>
+                                <h5>Fontend-Developer</h5>
+                                
+                            </div>
+                        </div>
+                    </article>
+                      
+                    <article class="team-member">
+                        <img src="{{asset('images/Nischal.jpg')}}" alt="team-member">
+                        <div class="img-footer">
+                            <div class="footer-info">
+                                <h3>Nischal Kharel(Groom)</h3>
+                                <h5>Backend-developer</h5>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
 @endsection
